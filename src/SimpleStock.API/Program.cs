82a5546ee21bc503fd1adb0cb9d69ddbf781ae75
Alexandre Lima <dev.alexandrelima@gmail.com>
2023-05-31 08:02:59 -0300

    fix: CORS policy

--- src/SimpleStock.API/Program.cs
+++ src/SimpleStock.API/Program.cs
@@ -71,6 +71,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowAnyOrigin());
+
 app.MapControllers();
 
 app.Run();
